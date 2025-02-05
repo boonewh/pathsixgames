7e44c34f96c3a421a39966683a3553c78676d161
William Boone <138145899+boonewh@users.noreply.github.com>
2025-02-04 22:45:21 -0600

    fixed errors from Blueprints

--- pathsixgames/__init__.py
+++ pathsixgames/__init__.py
@@ -13,7 +13,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail(app)
 
